c12f08eacce6c280a8791f83915cd70f911e4d0c
FireInjun <mk@fireinjun.com>
2018-11-15 00:45:37 -0500

    prettifying everything

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,32 +1,32 @@
-'use strict';
+"use strict";
 
-const Glue = require('glue');
-const Manifest = require('./manifest');
+const Glue = require("glue");
+const Manifest = require("./manifest");
 
-exports.deployment = async (start) => {
+exports.deployment = async start => {
+  const manifest = Manifest.get("/", process.env);
+  const server = await Glue.compose(
+    manifest,
+    { relativeTo: __dirname }
+  );
 
-    const manifest = Manifest.get('/', process.env);
-    const server = await Glue.compose(manifest, { relativeTo: __dirname });
+  await server.initialize();
 
-    await server.initialize();
-
-    if (!start) {
-        return server;
-    }
-
-    await server.start();
-
-    console.log(`Server started at ${server.info.uri}`);
-
+  if (!start) {
     return server;
+  }
+
+  await server.start();
+
+  console.log(`Server started at ${server.info.uri}`);
+
+  return server;
 };
 
 if (!module.parent) {
+  exports.deployment(true);
 
-    exports.deployment(true);
-
-    process.on('unhandledRejection', (err) => {
-
-        throw err;
-    });
+  process.on("unhandledRejection", err => {
+    throw err;
+  });
 }
